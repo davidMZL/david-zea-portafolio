7a944f4189de9fe6ce34ab2890d9ce80d2570ecd
davidMZL <davidzea@Zea>
2025-03-05 15:13:49 -0500

    Configurar GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/david-zea-portafolio/',
   plugins: [
     vue(),
     vueDevTools(),
